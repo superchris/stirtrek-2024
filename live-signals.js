28a8c07c6085e56264cf904294d1507446d2b532
Chris Nelson <superchrisnelson@gmail.com>
2024-04-26 13:57:12 -0400

    better preact signal

--- live-signals.js
+++ live-signals.js
@@ -3,13 +3,15 @@ import { Signal } from 'signal-polyfill';
 import LiveState from 'phx-live-state';
 import subscript from 'subscript';
 
-export const createPreactSignal = (liveState, initialValue, path) => {
-  const updater = subscript(path);
-  const preactSignal = signal(initialValue);
+export const createPreactSignal = (liveStateOrOptions) => {
+  const { path, initialValue } = liveStateOrOptions;
+  const liveState = createLiveState(liveStateOrOptions);
+  const updater = path? subscript(path) : (state) => state;
+  const preactSignal = signal(initialValue ? initialValue : {});
   liveState.eventTarget.addEventListener('livestate-change', ({detail: { state }}) => {
-    console.log('updating signal value', updater(state));
     preactSignal.value = updater(state);
   });
+  liveState.connect();
   return preactSignal;
 }
 
